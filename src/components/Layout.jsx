e483ee41218efb97c5e8d258fde3a1424aac1486
devsrikembang <devsrikembang@gmail.com>
2022-02-18 15:12:19 +0700

    2 files modified

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Header, Footer } from '.';
 import { PreloadProvider } from '../context/PreloadContext';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-3082LYXZJX');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default function Default({ children }) {
   return (
